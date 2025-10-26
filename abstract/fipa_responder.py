f44bca5d4f4552bc3e60b4d1405105e24879286c
tiagomcalves <271846427+tiagomcalves@users.noreply.github.com>
2025-10-26 20:27:53 +0000

    Refine Responder message handler

--- abstract/fipa_responder.py
+++ abstract/fipa_responder.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import inspect
 import threading
 import time
 import logging
@@ -27,15 +28,53 @@ class Responder(Agent):
 
         self.response_table = {}
 
-        # Iniciar o servidor em background
+        # new server thread
+        self.ready_event = threading.Event()
         self.thread = threading.Thread(target=self._run_server, daemon=True)
         self.thread.start()
 
     def _run_server(self):
         logger = logging.getLogger(f"uvicorn-thread-{self.port}")
         logger.setLevel(logging.INFO)
-        print(f"Running http server on host: {self.host} and on port {self.port}")
-        uvicorn.run(self.app, host=self.host, port=self.port, log_config=None, log_level="info")
+
+        config = uvicorn.Config(self.app, host=self.host, port=self.port, log_config=None, log_level="info")
+        server = uvicorn.Server(config)
+
+        # fires up another thread in the background and signal when ready
+        loop = threading.Thread(target=server.run, daemon=True)
+        loop.start()
+
+        # wait for the http srv to be ready
+        while not server.started:
+            pass
+
+        print(f"Running http server for \"{self.name}\" on host: {self.host} and on port {self.port}")
+        self.ready_event.set()  # signal main thread that the server is ready
+        loop.join()  # keeps main thread running
+
+    def call_command(self, cmd_name: str, *args, **kwargs):
+        """
+        Chama dinamicamente a função associada a cmd_name.
+        Passa args e kwargs para a função.
+        """
+        if cmd_name not in self.response_table:
+            return "Unknown request"
+
+        func = getattr(self, cmd_name, None)
+        if func is None:
+            raise ValueError(f"Comando desconhecido: {cmd_name}")
+
+        sig = inspect.signature(func)
+
+        # Filtrar kwargs válidos
+        valid_kwargs = {k: v for k, v in kwargs.items() if k in sig.parameters}
+
+        # Truncar args extras
+        param_list = list(sig.parameters.values())
+        positional_args_count = sum(1 for p in param_list if p.kind in (p.POSITIONAL_ONLY, p.POSITIONAL_OR_KEYWORD))
+        valid_args = args[:positional_args_count]
+
+        return func(*valid_args, **valid_kwargs)
 
     async def ingest(self, msg: AgentMessage):
         # Idempotency
@@ -52,12 +91,14 @@ class Responder(Agent):
 
         received_task_string = msg.content.get("task")
         task_handler = self.response_table.get(received_task_string)
+        #print("received task string: ", received_task_string)
+        #print("task_handler: ", task_handler)
 
         if task_handler is None:
             return {"status": "error", "code": "error.semantic.unknown_task"}
 
         # Simulate work
-        time.sleep(0.1)
+        #time.sleep(0.1)
 
         return {
             "status": "ok",
@@ -65,6 +106,6 @@ class Responder(Agent):
                 "performative": "inform",
                 "conversation_id": msg.conversation_id,
                 "in_reply_to": msg.message_id,
-                "summary": str(task_handler()),
+                "summary": str(self.call_command(received_task_string,msg.content.get("args"))),
             }
         }
--- sim.py
+++ sim.py
@@ -1,10 +1,12 @@
 from __future__ import annotations
+
+import uuid
 from typing import final
 
 import datetime
 import time
 
-import uvicorn
+import requests
 
 from env import Environment
 from abstract import *
@@ -12,6 +14,46 @@ from agent import *
 
 def now_iso():
     return datetime.datetime.now().replace(microsecond=0).isoformat() + "Z"
+
+def compose_request(conversation_id, sender, receiver, task, args):
+    return {
+        "message_id": str(uuid.uuid4()),
+        "conversation_id": conversation_id,
+        "sender": sender,
+        "receiver": receiver,
+        "timestamp": now_iso(),
+        "performative": "request",
+        "schema_version": "1.0.0",
+        "ttl": 120,
+        "idempotency_key": f"{conversation_id}:{task}",
+        "content": {"task": task, "args": args}
+    }
+
+def send(url, msg):
+    r = requests.post(url, json=msg, timeout=10)
+    r.raise_for_status()
+    return r.json()
+
+def available_lines():
+    msg = compose_request(conv, "planner@svc", "researcher@svc", "available_lines", "")
+    res = send("http://localhost:8000/ingest", msg)
+    query_res = res.get("reply").get("summary")
+    #print("\n", query_res)
+    print(query_res)
+
+def available_city_lines(input):
+    msg = compose_request(conv, "planner@svc", "researcher@svc", "available_city_lines", input)
+    res = send("http://localhost:8000/ingest", msg)
+    query_res = res.get("reply").get("summary")
+    #print("\n", query_res)
+    print(query_res)
+
+def malicious_available_lines(args):
+    msg = compose_request(conv, "planner@svc", "researcher@svc", "available_lines", args)
+    res = send("http://localhost:8000/ingest", msg)
+    query_res = res.get("reply").get("summary")
+    #print("\n", query_res)
+    print(query_res)
 
 @final
 class Simulator:
@@ -36,23 +78,35 @@ class Simulator:
 
 if __name__ == "__main__":
 
+    conv = "conv-{}".format(int(time.time()))
+
     curr_agents =[]
 
     travel_a = Agent.create("Travel Assistant", "agents.json")
+    travel_a.ready_event.wait()
+
     curr_agents.append(travel_a)
     env = Environment(5,curr_agents)
+
 
     while True :
 
         #print("My keys: ")
-        print(f"\n :: {now_iso()}" )
+        print(f" :: {now_iso()}" )
         string = env.agents[0].get_name()
 
-        print(string)
+        print(string)   # prints "Travel Assistant"
 
-        if isinstance(env.agents[0], TravelAssistant):
-            env.agents[0].available_lines()
+        #if isinstance(env.agents[0], TravelAssistant):
+        #    env.agents[0].available_lines()
 
         #render_state
 
-        time.sleep(2.0)
+        available_lines()
+        available_city_lines("Lisboa")
+
+        print("Calling \"available_lines()\" with extra parameters")
+
+        malicious_available_lines("garbage arg, 1, 123.456")
+
+        time.sleep(0.75)
