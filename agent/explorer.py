6929dad212d3b9067e07cec6f64d05d94d3867bb
Marevin <Marevin@users.noreply.github.com>
2025-11-21 19:08:24 +0000

    Correct return type to specify tuple of two ints

--- agent/explorer.py
+++ agent/explorer.py
@@ -10,7 +10,7 @@ class Explorer(Agent):
         starting_position = properties["starting_position"].split(",")
         self.position = ( int(starting_position[0]), int(starting_position[1]) )
 
-    def get_position(self) -> (int, int):
+    def get_position(self) -> tuple[int, int]:
         return self.position
 
     def move(self, direction: Direction) -> None:
