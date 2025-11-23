eacbf25cdda804ae9615ae9de709fe078a6fbd74
Marevin <Marevin@users.noreply.github.com>
2025-11-23 14:25:49 +0000

    Implement (r)add for Position+Position and Position+Direction

--- map/position.py
+++ map/position.py
@@ -1,12 +1,15 @@
 from __future__ import annotations
 
+from component.direction import Direction
+
+
 class Position:
 
     _pos : tuple[int, int]
 
     def __init__(self, x: int, y: int):
         if x < 0 or y < 0:
-            ValueError(f"Position cant have a coord below 0: {x} {y}")
+            raise ValueError(f"Position cant have a coord below 0: {x} {y}")
 
         self._pos = (x,y)
 
@@ -17,6 +20,19 @@ class Position:
         _other = other.get()
         return self._pos[0] == _other[0] and self._pos[1] == _other[1]
 
+    def __add__(self, other):
+        if isinstance(other, Position):
+            (ox, oy) = other.get()
+        elif isinstance(other, Direction):
+            (ox, oy) = other.value
+        else:
+            return NotImplemented
+
+        return Position(self._pos[0] + ox, self._pos[1] + oy)
+
+    def __radd__(self, other):
+        return self.__add__(other)
+
     def get(self) -> tuple[int,int]:
         return self._pos
 
@@ -25,6 +41,10 @@ class Position:
 
     def move(self, x:int, y:int):
         self._pos = (self._pos[0] + x, self._pos[1] + y)
+
+    def get_from_direction(self, direction:Direction):
+        return self + direction
+
 
     def is_strictly_less_than(self, other:Position):
         other_t = other.get()
