e9dfb189d22497b2ba83e50da4e53cdc80a06011
tiagomcalves <271846427+tiagomcalves@users.noreply.github.com>
2025-11-25 12:57:16 +0000

    Reorganize main.py, add --autostart option

--- main.py
+++ main.py
@@ -9,35 +9,42 @@ def main():
 
     directory = Path("problem/")
     subdirs = [f.name for f in directory.iterdir() if f.is_dir()]
-    #Multi Agents System
-    parser = argparse.ArgumentParser(
-                                     description='Abstract system of agents with different implementations',
+    
+    parser = argparse.ArgumentParser(description='Abstract multi-agents system with different implementations',
                                      epilog='LEI-PL 2025/26 - 106090,122123',
-                                     usage=f'%(prog)s <'+ '/'.join(subdirs) + '> [options]')
+                                     usage=f'%(prog)s problem [options]',
+                                     add_help=False)
+    
     # required arg
-    parser.add_argument("problem",choices=subdirs)
+    parser.add_argument('problem',choices=subdirs, help='simulation problem')
+
+    mutex_group = parser.add_mutually_exclusive_group()
 
     # optional args
+    parser.add_argument('-a', '--autostart', help='automatically start simulation', action='store_true')
+    mutex_group.add_argument('-l', '--headless', help='run without renderer (mutually exclusive with --step)', action='store_true')
+    mutex_group.add_argument('-s', '--step', default=750, help='set a step delay (in milliseconds) (default is 750ms)', nargs=1, type=int, metavar="ms")
     parser.add_argument('-t', '--train', help='training mode', action='store_true')
-    parser.add_argument('-v', '--verbose',help="Enable verbose output", action='store_true')
+    parser.add_argument('-v', '--verbose',help="enable verbose output", action='store_true')
 
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument('-s', '--step', default=750, help='step delay (in milliseconds)', type=int, metavar="ms")
-    group.add_argument('-l', '--headless', help='run without graphics (mutually exclusive with --step)', action='store_true')
 
     if len(sys.argv) == 1:
         parser.print_help()
         sys.exit(1)
-
+    
     args = parser.parse_args()
 
-    # got valid problem chosen
-    # process optionals
+    # got valid problem chosen, process optionals
+
     if args.train or args.headless or args.train:
         pass
 
     import_agents()
     sim = Simulator.create(args)
+
+    if not args.autostart:
+        input("Press <Enter> to start the simulation...")
+    
     sim.run()
 
 if __name__ == '__main__':
