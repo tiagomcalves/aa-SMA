250207a46ad8f62f747ca3ae7b35659c97e60d89
Marevin <tiago.alves017@gmail.com>
2025-11-25 21:55:42 +0000

    First draft of separate renderer class

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import argparse
 import sys
+import time
 from pathlib import Path
 
+from core.renderer import Renderer, render
 from core.sim import Simulator
 from core.module_importer import import_agents
-import core.logger
 from core.logger import Logger
 
 def main():
@@ -20,15 +21,13 @@ def main():
     # required arg
     parser.add_argument('problem',choices=subdirs, help='simulation problem')
 
-    mutex_group = parser.add_mutually_exclusive_group()
-
     # optional args
     parser.add_argument('-a', '--autostart', help='automatically start simulation', action='store_true')
-    mutex_group.add_argument('-l', '--headless', help='run without renderer (mutually exclusive with --step)', action='store_true')
-    mutex_group.add_argument('-s', '--step', default=750, help='set a step delay (in milliseconds) (default is 750ms)', type=int, metavar="ms")
+    parser.add_argument('-r', '--renderer', help='renders board in separate process', action='store_true')
+    parser.add_argument('-l', '--headless', help='run without renderer (mutually exclusive with --renderer and --step)', action='store_true')
+    parser.add_argument('-s', '--step', default=750, help='set a step delay (in milliseconds) (default is 750ms)', nargs=1, type=int, metavar="ms")
     parser.add_argument('-t', '--train', help='training mode', action='store_true')
     parser.add_argument('-v', '--verbose',help="enable verbose output", action='store_true')
-
 
     if len(sys.argv) == 1:
         parser.print_help()
@@ -38,6 +37,9 @@ def main():
 
     # got valid problem chosen, process optionals
 
+    if args.headless and (args.renderer or args.step):
+        raise AttributeError("Error: --headless is mutualy exclusive with --renderer or --step")
+
     Logger.initialize(args.verbose)
 
     if args.train or args.headless:
@@ -45,6 +47,23 @@ def main():
 
     import_agents()
     sim = Simulator.create(args)
+
+    Renderer.initialize()
+
+
+    render().send("hello")
+    render().send("how are you?")
+    render().send("this is cool")
+
+    # Read any output available
+    time.sleep(0.5)
+    while True:
+        line = render().read(timeout=0.1)
+        if not line:
+            break
+        print("[Main got]", line)
+
+    render().close()
 
     if not args.autostart:
         input("Press <Enter> to start the simulation...")
