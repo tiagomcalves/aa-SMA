691daab3722cbd8572a746be663c1d3934a0cc72
tiagomcalves <271846427+tiagomcalves@users.noreply.github.com>
2025-11-27 15:47:32 +0000

    Implement a cross-platform tkinter solution for a separate terminal-like window

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 import time
 from pathlib import Path
 
-from core.renderer import Renderer, render
+from core.renderer import Renderer
 from core.sim import Simulator
 from core.module_importer import import_agents
 from core.logger import Logger
@@ -25,7 +25,7 @@ def main():
     parser.add_argument('-a', '--autostart', help='automatically start simulation', action='store_true')
     parser.add_argument('-r', '--renderer', help='renders board in separate process', action='store_true')
     parser.add_argument('-l', '--headless', help='run without renderer (mutually exclusive with --renderer and --step)', action='store_true')
-    parser.add_argument('-s', '--step', default=750, help='set a step delay (in milliseconds) (default is 750ms)', nargs=1, type=int, metavar="ms")
+    parser.add_argument('-s', '--step', default=750, help='set a step delay (in milliseconds) (default is 750ms)',  type=int, metavar="ms")
     parser.add_argument('-t', '--train', help='training mode', action='store_true')
     parser.add_argument('-v', '--verbose',help="enable verbose output", action='store_true')
 
@@ -47,23 +47,6 @@ def main():
 
     import_agents()
     sim = Simulator.create(args)
-
-    Renderer.initialize()
-
-
-    render().send("hello")
-    render().send("how are you?")
-    render().send("this is cool")
-
-    # Read any output available
-    time.sleep(0.5)
-    while True:
-        line = render().read(timeout=0.1)
-        if not line:
-            break
-        print("[Main got]", line)
-
-    render().close()
 
     if not args.autostart:
         input("Press <Enter> to start the simulation...")
