7b89b58227688fc7b437217f054e01e9ded80da0
Marevin <Marevin@users.noreply.github.com>
2025-12-03 21:20:47 +0000

    Add new agent source files

---
+++ agent/ferb.py
@@ -0,0 +1,70 @@
+import random
+
+from abstract.agent import AgentState
+from abstract.nav2d import Navigator2D
+from component.action import Action
+from component.direction import Direction
+from component.observation import Observation, ObservationType
+from core.logger import log
+from map.position import Position
+
+
+class Ferb(Navigator2D):
+
+    def __init__(self, name: str, properties: dict):
+        super().__init__(name, properties)
+        self._position = Position(*properties["starting_position"])
+        self.char = properties["char"]
+        self.temp_mem_moves = []
+        self.visited_positions = []
+        self.last_move = Direction.NONE
+
+    def observation(self, obs: Observation):
+        if obs.type == ObservationType.ACCEPTED_MOVE:
+            self.curr_observations.clear()
+            self.temp_mem_moves.clear()
+            self.last_move = obs.payload.direction
+            self.temp_mem_moves.append(self.last_move.opposite())
+
+        if obs.type == ObservationType.TERMINATE:
+            self.state = AgentState.TERMINATED
+
+
+    def act(self) -> Action:
+        if not self.has_observations():
+            self.use_sensor()
+            #print(self.curr_observations)
+            return Action.wait(self)
+
+        obs_surr = self.curr_observations.get(ObservationType.SURROUNDINGS)
+
+        if obs_surr and obs_surr.payload.cells[Direction.NONE] == "OBJECTIVE":
+            return Action.pick(self)
+
+        mem_moves = self.temp_mem_moves  #reference, not a copy
+
+        print(f"{self.name} saved moves: {[str(move) for move in mem_moves]}")
+
+        obs_direc = self.curr_observations.get(ObservationType.DIRECTION)
+
+        if obs_direc:
+            x_direc, y_direc = obs_direc.payload.direction
+            if x_direc not in mem_moves:
+                mem_moves.append(x_direc)
+                return Action.move(self, x_direc)
+
+            if y_direc not in mem_moves:
+                mem_moves.append(y_direc)
+                return Action.move(self, y_direc)
+
+        if obs_surr:
+
+            while True:
+                option = random.choice(list(obs_surr.payload.cells))
+                if option not in mem_moves:
+                    break
+
+            mem_moves.append(option)
+            return Action.move(self, option)
+
+        return Action.wait(self)
