88100c88caed452a3b673f4d3ffb5eff8b4608da
Marevin <Marevin@users.noreply.github.com>
2025-12-05 11:58:32 +0000

    Clear unused imports and remove deprecated files
    deprecated files:
    - abstract/utils package
    - abstract/fipa_responder.py abstract knowledgeable agent
    - agent/travel_assistant.py prototype agent
    - component/sensor/request.py for requests, these are executed from Sensor class in sensor.py
    - onto/travel.owx ontology file

--- abstract/__init__.py
+++ abstract/__init__.py
@@ -1,5 +1,4 @@
 from .agent import Agent
-from .fipa_responder import Responder
 from .nav2d import Navigator2D
 
-__all__ = ["Agent", "Responder", "Navigator2D"]
+__all__ = ["Agent", "Navigator2D"]
